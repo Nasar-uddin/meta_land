72498154efb7a5a2fceb0d0818eeeebc42e03910
Nasar-uddin <nasar00708@gmail.com>
2022-07-02 22:08:53 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>
+          Hello Nasar uddin
+        </h1>
+      </>
+    );
+  }
+}
+
+export default hot(App);
